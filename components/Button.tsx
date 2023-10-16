d992c9784a69a03e9cc83ef53d8bef26fbc4f053
Jeetjyoti Deka <jeetjyoti2020@gmail.com>
2023-10-16 19:35:57 +0530

    Seventh Section done

--- components/Button.tsx
+++ components/Button.tsx
@@ -5,16 +5,21 @@ type ButtonProps = {
   title: string;
   icon?: string;
   variant: string;
+  full?: boolean;
 };
 
-const Button = ({ variant, type, title, icon }: ButtonProps) => {
+const Button = ({ variant, type, title, icon, full }: ButtonProps) => {
   return (
     <button
-      className={`flexCenter gap-3 rounded-full border ${variant}`}
+      className={`flexCenter gap-3 rounded-full border ${variant} ${
+        full && "w-full"
+      }`}
       type={type}
     >
       {icon && <Image src={icon} width={24} height={24} alt={title} />}
-      <label className="bold-16 whitespace-nowrap">{title}</label>
+      <label className="bold-16 whitespace-nowrap cursor-pointer">
+        {title}
+      </label>
     </button>
   );
 };
